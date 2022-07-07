3f37634e018c4cba2053838fd92c60a79e330afe
Gerardo Klos <gklos002@gmail.com>
2022-07-06 21:48:01 -0300

    commit menu design

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -1,24 +1,26 @@
 <header x-data="dropdown()"
 class="bg-neutral-700 sticky top-0">
 	{{-- Barra de navegacion --}}
-	<div class="container flex items-center h-16">
+	<div class="container flex items-center h-16 justify-between md:justify-start">
 		<a :class="{'bg-opacity-100 text-orange-500' : open}"
 		x-on:click="show()"
-		class="flex flex-col items-center justify-center px-4 bg-white bg-opacity-25 text-white cursor-pointer font-semibold h-full">
+		class="flex flex-col items-center justify-center order-last md:order-first px-6 md:px-4 bg-white bg-opacity-25 text-white cursor-pointer font-semibold h-full">
 			<!-- Hamburger -->
 			<svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
 				<path class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
 			</svg>
-			<span class="text-sm">Categroías</span>
+			<span class="text-sm hidden md:block">Categroías</span>
 		</a>
 		<a href="/" class="mx-6">
 			<x-jet-application-mark class="block h-9 w-auto" />
 		</a>
 
-		@livewire('search')
+		<div class="flex-1 hidden md:block">
+			@livewire('search')
+		</div>
 
 		<!-- Settings Dropdown -->
-		<div class="mx-6 relative">
+		<div class="mx-6 relative hidden md:block">
 			@auth
 				{{-- Cuando el usuario esta logeado --}}
 				<x-jet-dropdown align="right" width="48">
@@ -84,14 +86,17 @@ class="bg-neutral-700 sticky top-0">
 
 		</div>
 
-		@livewire('dropdown-cart')
+		<div class="hidden md:block">
+			@livewire('dropdown-cart')
+		</div>
 
 	</div>
 	{{-- Dropdown del boton categorías --}}
 	<nav :class="{ 'block': open, 'hidden': !open }"
 	id="navigation-menu"
 	class="bg-neutral-700 bg-opacity-25 w-full absolute hidden">
-		<div class="container h-full">
+		{{-- Menu para escritorio --}}
+		<div class="container h-full hidden md:block">
 			<div x-show='open' x-on:click.away="close()"
 			class="grid grid-cols-4 h-full relative">
 				<ul class="bg-white">
@@ -113,6 +118,59 @@ class="bg-neutral-700 sticky top-0">
 					<x-navigation-subcategories :category="$categories->first()" />
 				</div>
 			</div>
+		</div>
+		{{-- Menu para mobile --}}
+		<div class="bg-white h-full overflow-y-auto">
+			<div class="container bg-gray-200 py-3 mb-2">
+				@livewire('search')
+			</div>
+			<ul>
+				@foreach ($categories as $category)
+				<li class="text-neutral-500 hover:bg-orange-500 hover:text-white">
+					<a href="" class="py-2 px-4 text-sm flex items-center">
+						<span class="flex justify-center w-9">
+							{!! $category->icon !!}
+						</span>
+						{{ $category->name }}
+					</a>
+				</li>
+				@endforeach
+			</ul>
+			<p class="text-neutral-500 px-6 my-2">Usuario</p>
+			@livewire('cart-mobile')
+			@auth
+				<a href="{{ route('profile.show') }}" class="py-2 px-4 text-sm flex items-center text-neutral-500 hover:bg-orange-500 hover:text-white">
+					<span class="flex justify-center w-9">
+						<i class="fa-solid fa-address-card"></i>
+					</span>
+					Perfil
+				</a>
+				<a href=""
+				onclick="event.preventDefault();
+						document.getElementById('logout_form').submit();"
+				class="py-2 px-4 text-sm flex items-center text-neutral-500 hover:bg-orange-500 hover:text-white">
+					<span class="flex justify-center w-9">
+						<i class="fa-solid fa-right-from-bracket"></i>
+					</span>
+					Cerrar sesión
+				</a>
+				<form id="logout_form" action="{{ route('logout') }}" method="POST" hidden>
+					@csrf
+				</form>
+			@else
+				<a href="{{ route('login') }}" class="py-2 px-4 text-sm flex items-center text-neutral-500 hover:bg-orange-500 hover:text-white">
+					<span class="flex justify-center w-9">
+						<i class="fa-solid fa-arrow-right-to-bracket"></i>
+					</span>
+					Iniciar sesión
+				</a>
+				<a href="{{ route('register') }}" class="py-2 px-4 text-sm flex items-center text-neutral-500 hover:bg-orange-500 hover:text-white">
+					<span class="flex justify-center w-9">
+						<i class="fa-solid fa-address-card"></i>
+					</span>
+					Registrate
+				</a>
+			@endauth
 		</div>
 	</nav>
 </header>
